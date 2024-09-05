619336124d29fe9efb4606491f3feccc9a2c35a6
João Vitor <jvsrdez@hotmail.com>
2024-09-05 10:18:13 -0300

    Inicialização do projeto Spring Boot

---
+++ src/main/java/com/joaoramos/ajuda_certa/AjudaCertaApplication.java
@@ -0,0 +1,13 @@
+package com.joaoramos.ajuda_certa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaCertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaCertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoramos/ajuda_certa/AjudaCertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoramos.ajuda_certa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaCertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
